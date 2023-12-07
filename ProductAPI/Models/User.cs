0ed2c217220eb474c69cbbfaf140a21af80343f7
BaloyiTk <tiyanikevinbaloyi@gmail.com>
2023-12-07 23:46:05 +0200

    Added password field

--- ProductAPI/Models/User.cs
+++ ProductAPI/Models/User.cs
@@ -7,5 +7,8 @@ namespace ProductAPI.Models
        
         public int Id { get; set; }
         public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
     }
 }
